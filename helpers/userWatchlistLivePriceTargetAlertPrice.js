59dafa6d5728cc19db73a81d08bd96b455f2bf5c
csmurillo <csmurillo00@gmail.com>
2022-10-02 13:36:47 -0700

    env variables added

---
+++ helpers/userWatchlistLivePriceTargetAlertPrice.js
@@ -0,0 +1,140 @@
+const fetch = require('node-fetch');
+const { WatchList } = require('../models/watchList');
+require("dotenv").config();
+
+// function that looks up if stocks live price has reached alert price
+// if so save the date alert price was reached and
+// mark the alert price for the particular stock as reached.
+exports.userWatchlistLivePriceTargetAlertPrice= (userID)=>{    
+        WatchList.findOne({ owner: userID}, async (err, watchList) => {
+            // console.log('UserID: '+userID);
+            // console.log('Stocks: ');
+            const date = new Date();
+            const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+            const stocks=watchList.stocks;
+
+            for(let i =0; i<stocks.length;i++){        
+                // console.log(stocks[i]);
+                const stockSymbol=stocks[i].tickerSymbol;
+                // mock data
+                const finnhub=`https://mockstockapi.herokuapp.com/api/stockLivePrice?stock=${stockSymbol}`;
+                // live data
+                // const finnhub=`https://finnhub.io/api/v1/quote?symbol=${stockSymbol}&token=${process.env.STOCK_INFO_FINNHUB_API_KEY}`;
+                // const finnhubRes=await fetch(finnhub);
+                // const pricePromise=await finnhubRes.json();
+                // mock data
+                const finnhubRes=await fetch(finnhub);
+                const pricePromise=await finnhubRes.json();
+                // mock data
+                const stockPrice=pricePromise.price;
+                // live data
+                // const stockPrice=pricePromise.c;
+                const alertPrice=stocks[i].alertPrice;
+                if(!stocks[i].priceTargetReached){
+                    if(stocks[i].alertDirection==='above'){
+                        if(parseFloat(stockPrice)>parseFloat(alertPrice)){
+                            // console.log('stock ALERT fulfilled above'+stockSymbol+'USER: '+userID);
+                            // console.log('StockSymbol:'+stockSymbol);
+                            // console.log('stockPrice'+stockPrice+'Alerting at'+alertPrice);
+                            // console.log('DATE: '+(month+1)+'/'+day+'/'+year);
+                            stocks[i].priceTargetReached=true;
+                            stocks[i].datePriceTargetReached=(month+1)+'/'+day+'/'+year;
+                            
+                            
+                            const stock={
+                                tickerName:stocks[i].tickerName,
+                                tickerSymbol:stocks[i].tickerSymbol,
+                                priceAlert:stocks[i].alertPrice,
+                                alertDirection:stocks[i].alertDirection,
+                                datePriceTargetReached:stocks[i].datePriceTargetReached
+                            };
+                            // 
+                            const stockHistoryRes=fetch(`http://localhost:3003/api/add/to/stock/history?owner=${watchList.owner}`, {
+                                method: 'PUT',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    Accept: 'application/json',
+                                },
+                                body: JSON.stringify(stock)
+                            })
+                            .then(response => {
+                                return response.json();
+                            })
+                            .catch(err =>{ return err;});
+                            // 
+
+                            // console.log('server 2:(ABOVE)'+JSON.stringify(stockHistoryRes));
+                            // console.log('------------------------------------------------');
+
+                            // 
+                            // stocks[i].priceTargetReached=true;
+                            // stocks[i].datePriceTargetReached=month+'/'+day+'/'+year;
+                            // watchList.save((err,watchlist)=>{
+                            //     if(err){}
+                            // });
+                            // 
+                            watchList.stocks=watchList.stocks.filter(stock=>stock.tickerSymbol!=stocks[i].tickerSymbol);
+                            watchList.save((err,watchlist)=>{
+                                if(err){
+                                    console.log('error'+err);
+                                }
+                                console.log(watchlist);
+                            });
+                        }
+                    }
+                    else if(stocks[i].alertDirection==='below'){
+                        if(parseFloat(stockPrice)<parseFloat(alertPrice)){
+                            // console.log('stock ALERT fulfilled below'+'USER: '+userID);
+                            // console.log('StockSymbol:'+stockSymbol);
+                            // console.log('stockPrice'+stockPrice+'Alerting at'+alertPrice);
+                            // console.log('DATE: '+(month+1)+'/'+day+'/'+year);
+                            stocks[i].priceTargetReached=true;
+                            stocks[i].datePriceTargetReached=(month+1)+'/'+day+'/'+year;
+
+
+                            const stock={
+                                tickerName:stocks[i].tickerName,
+                                tickerSymbol:stocks[i].tickerSymbol,
+                                priceAlert:stocks[i].alertPrice,
+                                alertDirection:stocks[i].alertDirection,
+                                datePriceTargetReached:stocks[i].datePriceTargetReached
+                            };
+                            // 
+                            const stockHistoryRes=fetch(`http://localhost:3003/api/add/to/stock/history?owner=${watchList.owner}`, {
+                                method: 'PUT',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    Accept: 'application/json',
+                                },
+                                body: JSON.stringify(stock)
+                            })
+                            .then(response => {
+                                return response.json();
+                            })
+                            .catch(err =>{ return err;});
+                            // 
+                            
+                            // console.log('server 2(BELOW):'+JSON.stringify(stockHistoryRes));
+                            // console.log('------------------------------------------------');
+
+                            // 
+                            // stocks[i].priceTargetReached=true;
+                            // stocks[i].datePriceTargetReached=month+'/'+day+'/'+year;
+                            // watchList.save((err,watchlist)=>{
+                            // });
+                            // 
+                            watchList.stocks=watchList.stocks.filter(stock=>stock.tickerSymbol!=stocks[i].tickerSymbol);
+                            watchList.save((err,watchlist)=>{
+                                if(err){
+                                    console.log('error'+err);
+                                }
+                                console.log(watchlist);
+                            });
+                        }
+                    }
+                }
+
+            }
+        });
+};
+
--- server.js
+++ server.js
@@ -1,35 +1,89 @@
+const http = require('http');
 const express =require('express');
 const mongoose=require('mongoose');
 const app = express();
-const {userWatchlistTargetLivePrice}=require('./helpers/userWatchlistTargetLivePrice');
+const server = http.createServer(app);
+
+const {userWatchlistLivePriceTargetAlertPrice}=require('./helpers/userWatchlistLivePriceTargetAlertPrice');
+const {userWatchlist}=require('./helpers/userWatchlist');
 const {WatchList}=require('./models/watchList');
 
+const io = require('socket.io')(server,{
+    cors: {
+        origin: process.env.ORIGIN_URL
+    },
+});
+
+// socketio middleware
+io.use((socket, next) => {
+    const userID = socket.handshake.auth.id;
+    
+    socket.userID = userID;
+    next();
+});
+
+io.on('connection',async(socket)=>{
+
+    socket.on('checkLivePrices',async(stocks)=>{
+        WatchList.findOne({ owner: socket.userID}, async (err, watchList) => {
+            console.log(watchList);
+            socket.emit('updateWatchlistLivePrice',{liveWatchList:watchList.stocks.reverse()});
+        });
+    });
+    
+    socket.on('onWatchList',async({stockSymbol})=>{
+        // console.log('onWatchList');
+        let inWatchList=false;
+        WatchList.findOne({ owner: socket.userID}, (err, watchList) => {
+            const userWatchListStocks = watchList.stocks;
+            for(let stockList of userWatchListStocks){
+                const watchListSymbol=stockList.tickerSymbol;
+                console.log(stockList.priceTargetReached+'~+~');
+                console.log(watchListSymbol+'WATCHLIST');
+                console.log(stockSymbol+'STOCKSYMBOL');
+                if(stockSymbol===watchListSymbol){
+                    console.log('inside');
+                    console.log(stockList.priceTargetReached);
+                    if(!stockList.priceTargetReached){
+                        socket.emit('updateOnWatchList',{insideWatchList:true});
+                        inWatchList=true;
+                    }
+                }
+            }
+            console.log(inWatchList);
+            if(!inWatchList){
+                socket.emit('updateOnWatchList',{insideWatchList:false});
+            }
+            console.log('~~~~~~~~~~');
+        });
+    });
+});
+
+
+// end of io
+
 // connect to mongodb
-mongoose.connect()
+mongoose.connect(process.env.MONGO_URL)
         .then(()=>{console.log('db connected');})
         .catch(()=>{console.log('error db');});
 
-// stocks: check live target price fulfilled
+// stocks: check if a user watchlist stocks live target price reaches alertPrice
 setInterval(()=>{
-    // console.log('setInterval');
     WatchList.find((err, watchLists) => {
         if (err) {
             console.log('ERROR'+err)
         } else {
             for(let i=0; i<watchLists.length;i++){
-                userWatchlistTargetLivePrice(watchLists[i].owner);
+                userWatchlistLivePriceTargetAlertPrice(watchLists[i].owner,io);
             }
         }
     });
-    // console.log('-------------------------------------------------------');
-    // console.log('-------------------------------------------------------');
-    // console.log('-------------------------------------------------------');
 },10000)
 
-const PORT = process.env.PORT||3004;
+const PORT = process.env.PORT||3008;
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`Listening in port ${PORT}`);
 });
 
